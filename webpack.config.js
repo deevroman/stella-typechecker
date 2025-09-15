4b2c407876bceec4bed3c0ffe0ba98aa082b0d1a
Roman Deev <roman.deev06@gmail.com>
2025-09-15 14:57:55 +0300

    fix path

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ export default [{
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: './'
     },
     plugins: [
         new HtmlWebpackPlugin({
