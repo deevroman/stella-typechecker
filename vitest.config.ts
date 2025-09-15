2bd91fc143dc3edb51f2f69343660deb7b6dfda4
Roman Deev <roman.deev06@gmail.com>
2025-09-15 17:07:34 +0300

    git add

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+import {defineConfig} from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'],
+            reportsDirectory: './coverage',
+            reportOnFailure: true
+        },
+    },
+})
